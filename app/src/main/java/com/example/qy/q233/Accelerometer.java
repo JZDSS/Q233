f95ec9a894fc0b74281ca92aa2be9ae8b2541e61
QiYao <qy@stu.scu.edu.cn>
2017-03-15 20:25:55 +0800

    Class Accelerometer

---
+++ app/src/main/java/com/example/qy/q233/Accelerometer.java
@@ -0,0 +1,53 @@
+package com.example.qy.q233;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+/**
+ * Created by qy on 17-3-15.
+ */
+
+public class Accelerometer extends Activity{
+
+    public SensorManager mSensorManager;
+    private Sensor mSensor;
+    public MySensorEventListener mSensorEventListener;
+    public float x, y, z, norm;
+
+    public Accelerometer(Context mContext) {
+        mSensorManager = (SensorManager)mContext.getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorEventListener = new MySensorEventListener();
+    }
+
+    protected void resume() {
+        mSensorManager.registerListener(mSensorEventListener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void pause() {
+        mSensorManager.unregisterListener(mSensorEventListener);
+    }
+
+    private float norm(float x, float y, float z){
+        return (float)Math.sqrt(x * x + y * y + z * z);
+    }
+
+    private class MySensorEventListener implements SensorEventListener{
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
+            norm = norm(x, y, z);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
+}
--- app/src/main/java/com/example/qy/q233/MainActivity.java
+++ app/src/main/java/com/example/qy/q233/MainActivity.java
@@ -11,57 +11,58 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
-    private SensorEventListener mSensorEventListener;
+    //    private SensorManager mSensorManager;
+//    private Sensor mAccelerometer;
+//    private SensorEventListener mSensorEventListener;
     private boolean sensorOn;
-    //private static final String TAG = "SensorTest";
+    private Accelerometer mmAccelerometer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                TextView textView_x = (TextView) findViewById(R.id.val_x);
-                TextView textView_y = (TextView) findViewById(R.id.val_y);
-                TextView textView_z = (TextView) findViewById(R.id.val_z);
-                TextView textView_norm = (TextView) findViewById(R.id.val_norm);
-                float x = event.values[0];
-                float y = event.values[1];
-                float z = event.values[2];
-                float norm = norm(x, y, z);
-                textView_x.setText("" + x + "");
-                textView_y.setText("" + y + "");
-                textView_z.setText("" + z + "");
-                textView_norm.setText("" + norm + "");
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-        };
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        //mSensorManager.registerListener( mSensorEventListener, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+//        mSensorEventListener = new SensorEventListener() {
+//            @Override
+//            public void onSensorChanged(SensorEvent event) {
+//                TextView textView_x = (TextView) findViewById(R.id.val_x);
+//                TextView textView_y = (TextView) findViewById(R.id.val_y);
+//                TextView textView_z = (TextView) findViewById(R.id.val_z);
+//                TextView textView_norm = (TextView) findViewById(R.id.val_norm);
+//                float x = event.values[0];
+//                float y = event.values[1];
+//                float z = event.values[2];
+//                float norm = norm(x, y, z);
+//                textView_x.setText("" + x + "");
+//                textView_y.setText("" + y + "");
+//                textView_z.setText("" + z + "");
+//                textView_norm.setText("" + norm + "");
+//            }
+//
+//            @Override
+//            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//
+//            }
+//        };
+        //mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        //mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mmAccelerometer = new Accelerometer(this);
     }
 
+    @Override
     protected void onResume() {
         super.onResume();
         ((Button) findViewById(R.id.control)).setText(R.string.stop);
-        mSensorManager.registerListener(mSensorEventListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        //mSensorManager.registerListener(mSensorEventListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         sensorOn = true;
+        mmAccelerometer.resume();
     }
 
+    @Override
     protected void onPause() {
         super.onPause();
         ((Button) findViewById(R.id.control)).setText(R.string.start);
-        mSensorManager.unregisterListener(mSensorEventListener);
+        //mSensorManager.unregisterListener(mSensorEventListener);
         sensorOn = false;
+        mmAccelerometer.pause();
     }
 
     public void clickButton(View view){
@@ -73,12 +74,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private float norm(float x, float y, float z){
-        return (float)Math.sqrt(x * x + y * y + z * z);
-    }
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
+
+//    private float norm(float x, float y, float z){
+//        return (float)Math.sqrt(x * x + y * y + z * z);
+//    }
 
 }
