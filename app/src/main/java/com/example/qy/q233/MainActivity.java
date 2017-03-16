f0f01ea7e545b6f961f963117dc4d0b53a84525a
QiYao <qy@stu.scu.edu.cn>
2017-03-16 11:28:12 +0800

    timer中无法更新UI组件(refresh函数调用出错)，需用handler传递消息; 之前Accelerometer类中getSystemService出错，加入mContext后通过

--- app/src/main/java/com/example/qy/q233/MainActivity.java
+++ app/src/main/java/com/example/qy/q233/MainActivity.java
@@ -1,68 +1,60 @@
 package com.example.qy.q233;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class MainActivity extends AppCompatActivity {
-    //    private SensorManager mSensorManager;
-//    private Sensor mAccelerometer;
-//    private SensorEventListener mSensorEventListener;
+
     private boolean sensorOn;
-    private Accelerometer mmAccelerometer;
+    private Accelerometer mAccelerometer;
+    private Timer mTimer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        mSensorEventListener = new SensorEventListener() {
-//            @Override
-//            public void onSensorChanged(SensorEvent event) {
-//                TextView textView_x = (TextView) findViewById(R.id.val_x);
-//                TextView textView_y = (TextView) findViewById(R.id.val_y);
-//                TextView textView_z = (TextView) findViewById(R.id.val_z);
-//                TextView textView_norm = (TextView) findViewById(R.id.val_norm);
-//                float x = event.values[0];
-//                float y = event.values[1];
-//                float z = event.values[2];
-//                float norm = norm(x, y, z);
-//                textView_x.setText("" + x + "");
-//                textView_y.setText("" + y + "");
-//                textView_z.setText("" + z + "");
-//                textView_norm.setText("" + norm + "");
-//            }
-//
-//            @Override
-//            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-//
-//            }
-//        };
-        //mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        //mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mmAccelerometer = new Accelerometer(this);
+        mAccelerometer = new Accelerometer(this);
+        mTimer = new Timer();
+        TimerTask mTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                String x = String.valueOf(mAccelerometer.x);
+                refresh(R.id.val_x, x);
+            }
+        };
+        mTimer.schedule(mTimerTask,1000,5000);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         ((Button) findViewById(R.id.control)).setText(R.string.stop);
-        //mSensorManager.registerListener(mSensorEventListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         sensorOn = true;
-        mmAccelerometer.resume();
+        mAccelerometer.resume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         ((Button) findViewById(R.id.control)).setText(R.string.start);
-        //mSensorManager.unregisterListener(mSensorEventListener);
         sensorOn = false;
-        mmAccelerometer.pause();
+        mAccelerometer.pause();
+    }
+
+    @Override
+    protected void onDestroy () {
+        if (mTimer != null) {
+            mTimer.cancel( );
+            mTimer = null;
+        }
+        super.onDestroy();
     }
 
     public void clickButton(View view){
@@ -74,7 +66,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    private void refresh(int id, String s){
+        TextView mTextView = (TextView) findViewById(id);
+        mTextView.setText(s);
+    }
 //    private float norm(float x, float y, float z){
 //        return (float)Math.sqrt(x * x + y * y + z * z);
 //    }
