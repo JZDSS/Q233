a95c8e8bcde90a7a24978348f8ebdd26d5295363
QiYao <qy@stu.scu.edu.cn>
2017-03-19 00:19:57 +0800

    amap activity

--- app/src/main/java/com/example/qy/q233/AmapActivity.java
+++ app/src/main/java/com/example/qy/q233/AmapActivity.java
@@ -1,5 +1,6 @@
 package com.example.qy.q233;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,4 +8,11 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class AmapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amap);
+    }
+
 }
