cf447d6b2d5183f1e997245b12171fec7871db9a
QiYao <qy@stu.scu.edu.cn>
2017-03-19 00:19:16 +0800

    amap activity

---
+++ app/src/main/java/com/example/qy/q233/AmapActivity.java
@@ -0,0 +1,10 @@
+package com.example.qy.q233;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Qi Yao on 17-3-18.
+ */
+
+public class AmapActivity extends AppCompatActivity {
+}
