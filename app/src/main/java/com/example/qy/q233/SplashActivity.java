d80a878aa5b8f1d1f1431fdbf44fb182c81c914c
XuYining <oxuyining@gmail.com>
2017-03-28 20:21:20 +0800

    '界面加载动画'

--- app/src/main/java/com/example/qy/q233/SplashActivity.java
+++ app/src/main/java/com/example/qy/q233/SplashActivity.java
@@ -1,22 +1,31 @@
 package com.example.qy.q233;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 /**
  * Created by JZBB on 2017/3/27.
  */
 
 public class SplashActivity extends Activity {
-    private final int SPLASH_DISPLAY_LENGHT = 1000; // 延迟1秒
+    private final int SPLASH_DISPLAY_LENGHT = 3000; // 延迟3秒
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        TranslateAnimation animation = new TranslateAnimation(0, 0, 0, 0);
+        animation.setDuration(100);
+        animation.setFillAfter(true);
+        ImageView imageView = (ImageView) findViewById(R.id.loading);
+        ObjectAnimator.ofFloat(imageView, "rotation", 0F, 6000F).setDuration(100).start();
 
         new Handler().postDelayed(new Runnable() {
             public void run() {
@@ -29,4 +38,5 @@ public class SplashActivity extends Activity {
         }, SPLASH_DISPLAY_LENGHT);
 
     }
+
 }
