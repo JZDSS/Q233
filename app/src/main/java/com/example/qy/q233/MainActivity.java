dc1461169dccff6c2f4fe19eb3286681bee1f840
XuYining <oxuyining@gmail.com>
2017-04-06 20:39:50 +0800

    Jump

--- app/src/main/java/com/example/qy/q233/MainActivity.java
+++ app/src/main/java/com/example/qy/q233/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,11 +24,27 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button_jump1);
+        mButton.setOnClickListener(new ButtonListener());
     }
 
+    class ButtonListener implements OnClickListener {
 
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent();
+            //setClass函数的第一个参数是一个Context对象
+            //Context是一个类,Activity是Context类的子类,也就是说,所有的Activity对象都可以向上转型为Context对象
+            //setClass函数的第二个参数是Class对象,在当前场景下,应该传入需要被启动的Activity的class对象
+            intent.setClass(MainActivity.this, AccelerometerActivity.class);
+            startActivity(intent);
+        }
+    }
 }
