c55556223b438ea4b89cb83fdc5e8c16a090953f
XuYining <oxuyining@gmail.com>
2017-04-06 22:17:00 +0800

    aboutus格式问题(熟悉)

--- app/src/main/java/com/example/qy/q233/MainActivity.java
+++ app/src/main/java/com/example/qy/q233/MainActivity.java
@@ -32,10 +32,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButton = (Button) findViewById(R.id.button_jump1);
-        mButton.setOnClickListener(new ButtonListener());
+        mButton.setOnClickListener(new ButtonListener1());
+        mButton = (Button) findViewById(R.id.button_jump2);
+        mButton.setOnClickListener(new ButtonListener2());
+        mButton = (Button) findViewById(R.id.button_jump3);
+        mButton.setOnClickListener(new ButtonListener3());
     }
 
-    class ButtonListener implements OnClickListener {
+    class ButtonListener1 implements OnClickListener {
 
         @Override
         public void onClick(View v) {
@@ -44,6 +48,24 @@ public class MainActivity extends AppCompatActivity {
             //Context是一个类,Activity是Context类的子类,也就是说,所有的Activity对象都可以向上转型为Context对象
             //setClass函数的第二个参数是Class对象,在当前场景下,应该传入需要被启动的Activity的class对象
             intent.setClass(MainActivity.this, AccelerometerActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    class ButtonListener2 implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent();
+            intent.setClass(MainActivity.this, BDMapActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    class ButtonListener3 implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent();
+            intent.setClass(MainActivity.this, AboutusActivity.class);
             startActivity(intent);
         }
     }
